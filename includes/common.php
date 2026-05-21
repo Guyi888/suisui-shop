ad24c7330a9275419e3cbe3a2f6fc66426f4868e
suisui <qqfaka@users.noreply.github.com>
2026-05-21 11:12:31 +0800

    chore: bump cache version

--- includes/common.php
+++ includes/common.php
@@ -5,7 +5,7 @@ if (defined('IN_CRONLITE')) {
 	return;
 }
 define('CACHE_FILE', 0);
-define('VERSION', '2026052010');
+define('VERSION', '2026052101');
 define('IN_CRONLITE', true);
 define('tingdong', '3530793519');
 define('SYSTEM_ROOT', dirname(__FILE__) . '/');
